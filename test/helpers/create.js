841b2e5b85797fdf0b2ff484e7f04e2564e36b99
Andrew Osheroff <andrewosh@gmail.com>
2019-01-29 04:51:56 -0800

    Fixed valueEncoding in deletion bug (#6)
    * Fixed valueEncoding in deletion bug + a test
    
    * No spread operator

--- test/helpers/create.js
+++ test/helpers/create.js
@@ -1,6 +1,7 @@
 const ram = require('random-access-memory')
 const hypertrie = require('../../')
 
-module.exports = function (key) {
-  return hypertrie(ram, key, {valueEncoding: 'json'})
+module.exports = function (key, opts) {
+  opts = Object.assign({ valueEncoding: 'json' }, opts)
+  return hypertrie(ram, key, opts)
 }
