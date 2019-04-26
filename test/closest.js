41d12f90f0914b3106921d2d31afdd66112aadbb
Andrew Osheroff <andrewosh@gmail.com>
2019-04-26 16:01:12 +0200

    Updated flags handling + added  options (undocumented)

---
+++ test/closest.js
@@ -0,0 +1,48 @@
+const tape = require('tape')
+const create = require('./helpers/create')
+
+tape('closest put node is a prefix', function (t) {
+  t.plan(4)
+
+  const db = create()
+
+  db.put('a', 'hello', err => {
+    t.error(err, 'no error')
+    db.put('b', 'goodbye', err => {
+      t.error(err, 'no error')
+      db.put('a/b', 'something', {
+        closest: true,
+        condition: (closest, newNode, cb) => {
+          t.same(closest.key, 'a')
+          return cb(null, true)
+        }
+      }, err => {
+        t.error(err, 'no error')
+      })
+    })
+  })
+})
+
+tape('closest put node is a prefix, batch insertion with flags', function (t) {
+  t.plan(3)
+
+  const db = create()
+
+  db.batch([
+    { type: 'put', key: 'a', value: 'hello', flags: 1 },
+    { type: 'put', key: 'b', value: 'goodbye' }
+  ], err => {
+    t.error(err, 'no error')
+    db.put('a/b', 'something', {
+      closest: true,
+      condition: (closest, newNode, cb) => {
+        t.same(closest.key, 'a')
+        return cb(null, true)
+      }
+    }, err => {
+      t.error(err, 'no error')
+    })
+  })
+})
+
+// TODO: A test with a trie with one hidden entry, then do closest
