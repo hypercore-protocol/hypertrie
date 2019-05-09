b47202f962d84e7ee4aed31c5a40c47d5616a14f
Andrew Osheroff <andrewosh@gmail.com>
2019-05-09 23:39:44 +0200

    Basic deletion with closest option

--- test/closest.js
+++ test/closest.js
@@ -45,6 +45,32 @@ tape('closest put node is a prefix, batch insertion with flags', function (t) {
   })
 })
 
+tape.only('closest node to non-existing delete path is a prefix', function (t) {
+  t.plan(4)
+
+  const db = create()
+
+  db.batch([
+    { type: 'put', key: 'a', value: 'hello' },
+    { type: 'put', key: 'b', value: 'goodbye' },
+    { type: 'put', key: 'a/a', value: 'world' },
+    { type: 'put', key: 'b/a', value: 'dog' },
+    { type: 'put', key: 'b/b', value: 'otter' },
+  ], err => {
+    t.error(err, 'no error')
+    db.del('a/a/b', {
+      closest: true,
+      condition: (closest, cb) => {
+        t.same(closest.key, 'a/a')
+        return cb(null, true)
+      }
+    }, (err, node) => {
+      t.error(err, 'no error')
+      t.false(node)
+    })
+  })
+})
+
 tape('closest node with one hidden node', function (t) {
   t.plan(3)
 
@@ -53,7 +79,6 @@ tape('closest node with one hidden node', function (t) {
   db.put('a', 'hello', { hidden: true }, err => {
     t.error(err, 'no error')
     db.get('b', { closest: true }, (err, node) => {
-      console.log('node:', node)
       t.error(err, 'no error')
       t.false(node)
     })
