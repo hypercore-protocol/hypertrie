7fda8172b6adc5c70a1f47a3f7ae9e7a7086572c
Mathias Buus <mathiasbuus@gmail.com>
2020-06-10 16:17:48 +0200

    make create injectable

--- test/helpers/create.js
+++ test/helpers/create.js
@@ -2,6 +2,10 @@ const ram = require('random-access-memory')
 const hypertrie = require('../../')
 
 module.exports = function (key, opts) {
+  return module.exports.create(key, opts)
+}
+
+module.exports.create = function (key, opts) {
   opts = Object.assign({ valueEncoding: 'json' }, opts)
   return hypertrie(ram, key, opts)
 }
